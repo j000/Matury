f0f8deba5749e09c411cda4ae5aead99511cdd87
j000 <jarymut+github@gmail.com>
2017-03-05 12:50:41 +0100

    Matura 2016, nowa: Popraw zadanie 6.2

--- 2016/nowa/zadanie6.2.cpp
+++ 2016/nowa/zadanie6.2.cpp
@@ -1,5 +1,8 @@
 #include <string>
-#include <fstream>
+#include <fstream> // ifstream, ofstream
+#include <iostream> // cerr, endl
+
+using std::endl; // endl zamiast std::endl
 
 int main() {
 	std::ifstream dane;
@@ -7,10 +10,16 @@ int main() {
 	//////////
 	dane.open("dane_6_2.txt"); // otwieramy dane
 	odpowiedz.open("wyniki_6_2.txt"); // otwieramy wyniki
-	for (int i = 1; i <= 100; ++i) {
+	for (int i = 1; i <= 3000; ++i) {
 		std::string tmp;
 		int klucz;
 		dane >> tmp >> klucz;
+		if (dane.rdstate() != 0) { // czy nie ma błedów
+			dane.clear(); // wyczyść błąd
+			odpowiedz << "Błąd w linii " << i << "!" << endl;
+			std::cerr << "Błąd w linii " << i << "!" << endl;
+			continue;
+		}
 		klucz %= 26;
 		klucz = 26 - klucz; // szyfrowanie jest cykliczne, tak bedzie nam latwiej
 		for (int j = 0; j < tmp.size(); ++j) {
@@ -20,7 +29,7 @@ int main() {
 			k += 'A'; // zamieniamy na litere
 			odpowiedz << k;
 		}
-		odpowiedz << std::endl;
+		odpowiedz << endl;
 	}
 	// zamykamy pliki
 	odpowiedz.close();
